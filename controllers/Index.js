fadb401c28eac11916edb180606d071849039acb
赖汉彬 <humandetail@laihanbindeMacBook-Pro.local>
2020-09-27 13:43:12 +0800

    修改url值获取方式

--- controllers/Index.js
+++ controllers/Index.js
@@ -22,17 +22,17 @@ class IndexController {
       ticket
     } = await getTicket(accessToken);
 
-    // let {
-    //   protocol,
-    //   host,
-    //   url
-    // } = ctx;
+    let {
+      protocol,
+      host,
+      url
+    } = ctx;
 
-    // url = `${protocol}://${host}${url}`;
+    url = `${protocol}://${host}${url}`;
 
     const noncestr = createRandomStr(), // 随机字符串
-          timestamp = new Date().getTime(), // 时间戳
-          url = ctx.url;
+          timestamp = new Date().getTime(); // 时间戳
+          // url = ctx.url;
 
     // 生成签名，并使用sha1加密
     const signature = sha1(`jsapi_ticket=${ticket}&noncestr=${noncestr}&timestamp=${timestamp}&url=${url}`);
