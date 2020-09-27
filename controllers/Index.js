5bc8c926530062d860d9227cff250c3b1ad3989e
赖汉彬 <humandetail@laihanbindeMacBook-Pro.local>
2020-09-27 13:57:51 +0800

    .

--- controllers/Index.js
+++ controllers/Index.js
@@ -37,6 +37,10 @@ class IndexController {
     // 生成签名，并使用sha1加密
     const signature = sha1(`jsapi_ticket=${ticket}&noncestr=${noncestr}&timestamp=${timestamp}&url=${url}`);
 
+    //jsapi_ticket=sM4AOVdWfPE4DxkXGEs8VMCPGGVi4C3VM0P37wVUCFvkVAy_90u5h9nbSlYy3-Sl-HhTdfl2fzFy1AOcHKP7qg&noncestr=Wm3WZYTPz0wzccnW&timestamp=1414587457&url=http://mp.weixin.qq.com?params=value
+
+    console.log(ticket, noncestr, timestamp, url);
+
     ctx.body = {
       err_code: 0,
       message: 'success',
